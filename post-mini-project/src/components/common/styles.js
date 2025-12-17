9926895161b99f8bc1d5dbba9a969ef330f97102
JeawonYang <yjy5784@daum.net>
2025-12-17 17:44:39 +0900

    board

--- post-mini-project/src/components/common/styles.js
+++ post-mini-project/src/components/common/styles.js
@@ -20,10 +20,120 @@ export const frame = css`
 `;
 
 export const frameContainer = css`
+    position: relative;
     box-sizing: border-box;
     border-radius: 10px;
     width: 100%;
     height: 100%;
     background-color: #f3f5f7;
     overflow: hidden;
+`;
+
+
+
+/////////// Loading styles ///////////
+export const loadingBox = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 99;
+    width: 100%;
+    height: 100%;
+    background-color: #00000066;
+`;
+
+/////////// <<LeftSideBar>> ///////////
+export const leftSideBar = css`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    box-sizing: border-box;
+`;
+
+export const sideBarContainer = css`
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    height: 100%;
+    background-color: #ffffff;
+
+    & > h1 {
+        margin: 20px;
+        font-size: 20px;
+        text-align: center;
+        text-shadow: 0 0 20px #00000099;
+        cursor: default;
+        transition: all 0.3s ease-in-out;
+        &:hover {
+            transform: scale(105%);
+        }
+    }
+
+    & > ul {
+        list-style-type: none;
+        display: flex;
+        flex-direction: column;
+        padding: 0;
+        margin: 0;
+        gap: 8px;
+        flex-grow: 1;
+        
+        & > a {
+            text-decoration: none;
+            color: #222222;
+            
+        }
+    }
+
+    & > div {
+        display: flex;
+        justify-content: center;
+        padding: 20px;
+
+        & > a {
+            text-decoration: none;
+            color: #222222;
+            font-weight: 500;
+            text-shadow: 0 0 15px #000000;
+        }
+    }
+`;
+
+export const menuListItem = (isSelected) => css`
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 5px 15px;
+    width: 100%;
+    height: 35px;
+    font-weight: 500;
+    transition: all 0.1s ease-in-out;
+    box-shadow: 0 0 ${isSelected ? "10px 10px" : "0 0"} #0000000f inset;
+    &:hover {
+        box-shadow: 0 0 15px #00000066;
+    }
+
+    & > div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-right: 5px;
+        width: 30px;
+        height: 100%;
+        font-size: 20px;
+    }
+`;
+
+export const profileImg = (url) => css`
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    overflow: hidden;
+    background-image: url(${url});
+    background-position: center;
+    background-size: cover;
 `;
